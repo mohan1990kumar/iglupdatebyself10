2c689844c53cb98b4a90f25c06a38c86362dcdff
mohan1990kumar <mohan1990kumars@gmail.com>
2026-07-17 07:32:27 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+// initialize firebase
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBDjnmlOmYEN0GvZIFLUiCqZ3lFFYeFHmg",
+  authDomain: "k811by.firebaseapp.com",
+  databaseURL: "https://k811by-default-rtdb.firebaseio.com",
+  projectId: "k811by",
+  storageBucket: "k811by.firebasestorage.app",
+  messagingSenderId: "142648329051",
+  appId: "1:142648329051:web:6b23d4d1d401bb80dc507e",
+  measurementId: "G-QF3T28YPF0"
+};
+
+
+
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
